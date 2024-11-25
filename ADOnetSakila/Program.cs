257c1305020c8e86e62f4a7258275e838bc102ba
ohass <ohass@OSCAR>
2024-11-25 15:57:42 +0100

    Add project files.

---
+++ ADOnetSakila/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADOnetSakila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
